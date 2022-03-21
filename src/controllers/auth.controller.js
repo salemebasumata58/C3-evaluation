a319d035eb27b959b43670f90e91b670e386e7c2
salemes58 <sbasumatas58@gmail.com>
2022-03-21 10:42:29 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,33 +31,4 @@ const register = async (req, res) => {
     }
 }
 
-
-const login = async (req, res) => {
-    try{
-        
-        const user = await User.findOne({email : req.body.email})
-        //checked if mail exists
-        if(!user){
-            return res.status(400).send("Wrong Email or Password")
-        }
-
-        //if email exists, check password;
-        const match = user.checkPassword(req.body.password)
-
-        // if it doesn't match
-        if(!match){
-            return res.status(400).send({message : "Wrong Email or Password"})
-        }
-
-        // if it matches
-        const token = generateToken(user)
-        return res.status(200).send({user, token});
-
-
-    }
-    catch(err){
-        res.status(400).send({message : err.message})
-    }
-}
-
-module.exports = {register,login}
+module.exports = {register};
